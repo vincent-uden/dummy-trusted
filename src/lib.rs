501c57aae22bb245b53a48cdf510ad7baa6f9562
Vincent Udén <vincentuden@gmail.com>
2025-10-18 18:27:52 +0200

    Add mupdf-rs as dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,17 @@
+use std::path::Path;
+
+use mupdf::Document;
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
 pub fn sub(left: u64, right: u64) -> u64 {
     left - right
+}
+
+pub fn open_pdf(path: &Path) {
+    let doc = Document::open(path);
 }
 
 #[cfg(test)]
